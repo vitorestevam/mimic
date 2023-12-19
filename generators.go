6da5b0c68cf93d423b39e56e22858ff5e760911f
vitor estevam <vitor.estevam@hp.com>
2023-12-19 16:57:44 -0300

    [fix] generate lower case uuids

--- generators.go
+++ generators.go
@@ -4,6 +4,7 @@ import (
 	crypto "crypto/rand"
 	"fmt"
 	"math/rand"
+	"strings"
 	"time"
 )
 
@@ -22,5 +23,5 @@ func GetRandomUUID() string {
 
 	uuid := fmt.Sprintf("%X-%X-%X-%X-%X", b[0:4], b[4:6], b[6:8], b[8:10], b[10:])
 
-	return uuid
+	return strings.ToLower(uuid)
 }
