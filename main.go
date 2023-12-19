a9a3affd03974a393ebc3c3b6267095cc7cb9661
vitor estevam <vitor.estevam@hp.com>
2023-12-19 16:58:13 -0300

    [feat] pass files and rules by flags

--- main.go
+++ main.go
@@ -1,38 +1,49 @@
 package main
 
 import (
-	"fmt"
-	"log"
+	"flag"
+	"os"
+	"strings"
 )
 
-var Log string = `2023-04-24T10:15:27.123Z INFO kubelet: Starting container "web-server" with UUID "f9cc9ef3-3c62-4f51-bd29-1b1cb87f0ec8" in pod "my-app-1234-5678" with UUID "b791c38d-48eb-42c1-a212-2f8cf141d25e" on node "worker-1" with UUID "7fb18790-6d05-49c3-bdd3-73a3125e6b5e"
-2023-04-24T10:15:27.456Z INFO kubelet: Container "web-server" with UUID "f9cc9ef3-3c62-4f51-bd29-1b1cb87f0ec8" in pod "my-app-1234-5678" with UUID "b791c38d-48eb-42c1-a212-2f8cf141d25e" is running on node "worker-1" with UUID "7fb18790-6d05-49c3-bdd3-73a3125e6b5e"
-2023-04-24T10:15:32.789Z ERROR kubelet: Failed to pull image "my-registry/my-app:latest" for container "web-server" with UUID "f9cc9ef3-3c62-4f51-bd29-1b1cb87f0ec8" in pod "my-app-1234-5678" with UUID "b791c38d-48eb-42c1-a212-2f8cf141d25e" on node "worker-1" with UUID "7fb18790-6d05-49c3-bdd3-73a3125e6b5e": ImagePullBackOff
-2023-04-24T10:15:32.789Z WARN kubelet: Restarting container "web-server" with UUID "f9cc9ef3-3c62-4f51-bd29-1b1cb87f0ec8" in pod "my-app-1234-5678" with UUID "b791c38d-48eb-42c1-a212-2f8cf141d25e" on node "worker-1" with UUID "7fb18790-6d05-49c3-bdd3-73a3125e6b5e"
-2023-04-24T10:15:33.234Z INFO kubelet: Starting container "web-server" with UUID "3d85e9af-70a5-4aa2-aa2d-10a30b57c66b" in pod "my-app-1234-5678" with UUID "b791c38d-48eb-42c1-a212-2f8cf141d25e" on node "worker-2" with UUID "e0eb24df-8e77-4712-8d6c-87d2c8bf0e32"
-`
-
 func main() {
-	types := []string{"date", "uuid"}
+	var fFile = flag.String("f", "", "file path")
+	var fRules = flag.String("r", "date uuid", "rules file path")
+	flag.Parse()
+
+	if fFile == nil || *fFile == "" {
+		panic("file path (-f) is required")
+	}
+
+	rules := strings.Split(*fRules, " ")
+
+	file, err := os.ReadFile(*fFile)
+	if err != nil {
+		panic(err)
+	}
+
+	text := string(file)
 
 	replacers := []Replacer{}
 
-	for _, t := range types {
-		replacer, err := GetReplacers(t)
+	for _, rule := range rules {
+		replacer, err := GetReplacers(rule)
 		if err != nil {
-			log.Println(err)
+			panic(err)
 		}
 
 		replacers = append(replacers, replacer)
 	}
 
-	text := Log
-
-	fmt.Println(text)
-
+	new_text := text
 	for _, r := range replacers {
-		text = ApplyReplacer(r, text)
+		new_text = ApplyReplacer(r, new_text)
 	}
 
-	fmt.Println(text)
+	f, err := os.Create("./output.txt")
+	if err != nil {
+		panic(err)
+	}
+
+	f.Write([]byte(new_text))
 }
